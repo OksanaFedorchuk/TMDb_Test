b5237f2f28984cfcea9e19a9ef587e2075b29730
Oksana <klassi4eske@gmail.com>
2021-02-02 15:47:21 +0200

    Initial Commit

---
+++ TMDb_Test/TMDb_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDb_TestApp.swift
+//  TMDb_Test
+//
+//  Created by Oksana Fedorchuk on 02.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct TMDb_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
